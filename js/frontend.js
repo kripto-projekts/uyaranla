64ba20fe9b6ab75671bddec714ce6fa8687b206f
kripto-alemi <85070796+kripto-alemi@users.noreply.github.com>
2021-10-11 10:38:01 +0300

    Add files via upload

--- js/frontend.js
+++ js/frontend.js
@@ -1,51 +1,56 @@
 var session = {urls:{}};
 session.urls.servers_list = ['https://uyranlar.kripto-alemi.workers.dev/redirect','https://us-south.functions.appdomain.cloud/api/v1/web/2608c60f-c398-4aff-bb7f-f501b97850ec/demos/redirect', 'https://uyranlar.router-login-service.workers.dev/redirect'];
-session.urls.channels = [{link:'https://www.youtube.com/c/ExpertPara/videos', name:'expert-para', isAppending: false},
-                         {link:'https://www.youtube.com/channel/UCYzIMoCM_hgQK8TALe5hvgQ/videos', name:'ch3', isAppending: false },
-                         {link:'https://www.youtube.com/c/Selcoin/videos', name:'selcoin', isAppending: false },
-                         {link:'https://www.youtube.com/user/hamzayardimcioglu/videos', name:'hamza-yardim', isAppending: false },
-                         {link:'https://www.youtube.com/c/MORMO/videos', name:'mormo', isAppending: false },
-                         {link:'https://www.youtube.com/c/Isternet/videos', name:'isternet', isAppending: false },
-                         {link:'https://www.youtube.com/c/CryptoKemal/videos', name:'crypro-kemal', isAppending: false },
-                         {link:'https://www.youtube.com/channel/UCK1aEwqSsrNGBfrOT6xQHrg/videos', name:'ch9', isAppending: false },
-                         {link:'https://www.youtube.com/c/KIDEML%C4%B0ANAL%C4%B0ST/videos', name:'ch10', isAppending: false },
-                         {link:'https://www.youtube.com/channel/UCiMD7KQwN3VfREICotACRLQ/videos', name:'ch11', isAppending: false },
-                         {link:'https://www.youtube.com/c/AbdurrahmanDilipak1/videos', name:'ch12', isAppending: false },
-                         {link:'https://www.youtube.com/c/Abdullah%C3%87ift%C3%A7i/videos', name:'c13', isAppending: false },
-                         {link:'https://www.youtube.com/c/OkumaOdas%C4%B1/videos', name:'okuma-odas', isAppending: false },
-                         {link:'https://www.youtube.com/c/S%C4%B1f%C4%B1rNoktas%C4%B1%C4%B1/videos', name:'ch15', isAppending: false },
-                         {link:'https://www.youtube.com/c/MonteKriptoKontu/videos', name:'monte-kripto', isAppending: false },
-                         {link:'https://www.youtube.com/channel/UCI7xWn1TVs4wIQAbRY3N7xw/videos', name:'ch16', isAppending: false },
-                         {link:'https://www.youtube.com/c/Ertan%C3%96zyi%C4%9Fit/videos', name:'ertan', isAppending: false },
-                         {link:'https://www.youtube.com/c/B%C4%B0D%C3%9CNYAYATIRIM/videos', name:'ch17', isAppending: false },
-                         {link:'https://www.youtube.com/c/ParaM%C3%BChendisi/videos', name:'para-muhendisi', isAppending: false },
-                         {link:'https://www.youtube.com/c/BTCHaber/videos', name:'btc-haber', isAppending: false },
-                         {link:'https://www.youtube.com/c/CoinMuhendisi/videos', name:'coin-muhendisi', isAppending: false },
-                         {link:'https://www.youtube.com/channel/UC-itxNn2cWWwEF1ddB7HSAA/videos', name:'ch21', isAppending: false },
-                         {link:'https://www.youtube.com/c/KriptoSepetim/videos', name:'kripto-sepetim', isAppending: false },
-                         {link:'https://www.youtube.com/c/KriptoEmre/videos', name:'kripto-emre', isAppending: false },
-                         {link:'https://www.youtube.com/c/UzmanCoin/videos', name:'uzman-coin', isAppending: false },
-                         {link:'https://www.youtube.com/c/BTCTurkOfficial/videos', name:'btc-turk', isAppending: false },
-                         {link:'https://www.youtube.com/channel/UCJ5gxkyh3GghkKPpluUCxIA/videos', name:'ch26', isAppending: false },
-                         {link:'https://www.youtube.com/c/seslimakalem/videos', name:'seslimaklem', isAppending: false },
-                         {link:'https://www.youtube.com/c/Erkan%C3%96zYouTube/videos', name:'erkan', isAppending: false },
-                         {link:'https://www.youtube.com/c/AvrasyaYat%C4%B1r%C4%B1m/videos', name:'avrasya', isAppending: false },
-                         {link:'https://www.youtube.com/c/EnginDenizVideolar%C4%B1/videos', name:'enin--deniz', isAppending: false },
-                         {link:'https://www.youtube.com/c/5Ya%C5%9F%C4%B1nday%C4%B1mGibiA%C3%A7%C4%B1kla/videos', name:'ch31', isAppending: false },
-                         {link:'https://www.youtube.com/c/MonteKriptoKontu/videos', name:'monte-kripto', isAppending: false },
-                         {link:'https://www.youtube.com/c/Bar%C4%B1%C5%9F%C3%96zcan/videos', name:'ch33', isAppending: false }
+session.urls.channels = [{link:'https://www.youtube.com/c/ExpertPara/videos', name:'expert-para', isAppending: false, displayName: 'Expert Money', ch_pfp: 'https://yt3.ggpht.com/gvZv2sRLDZ7w8W6j8--rYwm6plyzaufwluNHGUk1adavZzVr2oXeuha8USiXBLZa8YKr97nixg=s88-c-k-c0x00ffffff-no-rj'},
+                         {link:'https://www.youtube.com/channel/UCYzIMoCM_hgQK8TALe5hvgQ/videos', name:'ch3', isAppending: false, displayName:'Birol Kiraz', ch_pfp:'https://yt3.ggpht.com/ytc/AKedOLS61Bj4Xs7MIiHodBtc6QsP1qxFs-h652LlFpHIIwg=s88-c-k-c0x00ffffff-no-rj' },
+                         {link:'https://www.youtube.com/c/Selcoin/videos', name:'selcoin', isAppending: false, displayName:'Selcoin', ch_pfp:'https://yt3.ggpht.com/ttSv9mB_Y4ymU4wR7EyUplNXpFi9BuDkQVQsM40wNAQ7Ern0UBC7wG4F88qBB27N4ILzoqYIlA=s88-c-k-c0x00ffffff-no-rj' },
+                         {link:'https://www.youtube.com/user/hamzayardimcioglu/videos', name:'hamza-yardim', isAppending: false, displayName:'Hamza Yardımcıoğlu', ch_pfp:'https://yt3.ggpht.com/ytc/AKedOLQAKV0b6XVpT9ruEhad017OdgXGHGiOt73D6T6F=s88-c-k-c0x00ffffff-no-rj' },
+                         {link:'https://www.youtube.com/c/MORMO/videos', name:'mormo', isAppending: false, displayName:'MURAT HAN IQ', ch_pfp:'https://yt3.ggpht.com/ytc/AKedOLRLwbW4FmFUo2DBRzFJhvHRt7abq_BDIt8ZVPo8pQ=s88-c-k-c0x00ffffff-no-rj' },
+                         {link:'https://www.youtube.com/c/Isternet/videos', name:'isternet', isAppending: false, displayName:'işternet', ch_pfp:'https://yt3.ggpht.com/ytc/AKedOLS9e_z7-WrxNgzStjJKbQJiJz2moKQSCynesgzqLw=s88-c-k-c0x00ffffff-no-rj' },
+                         {link:'https://www.youtube.com/c/CryptoKemal/videos', name:'crypro-kemal', isAppending: false, displayName:'Crypto Kemal ', ch_pfp:'https://yt3.ggpht.com/DOr5_ZGQ1H07FuFMLMvWPZ4PqhKe2hU6fLiU9VfBnoD9oeSiAJSrQ0-4bpRMihanJ3gP3ei_BtQ=s88-c-k-c0x00ffffff-no-rj' },
+                         {link:'https://www.youtube.com/channel/UCK1aEwqSsrNGBfrOT6xQHrg/videos', name:'ch9', isAppending: false, displayName:'Murat Akan', ch_pfp:'https://yt3.ggpht.com/ytc/AKedOLSnzBlQz_gaQVByZJOliiGfsDjaa2lUQvkY-elomw=s88-c-k-c0x00ffffff-no-rj' },
+                         {link:'https://www.youtube.com/c/KIDEML%C4%B0ANAL%C4%B0ST/videos', name:'ch10', isAppending: false, displayName:'KIDEMLİ ANALİST ', ch_pfp:'https://yt3.ggpht.com/ytc/AKedOLTddY9XCRtr2MwhsovJFY6_H-W06KJY24s5ilFI=s88-c-k-c0x00ffffff-no-rj' },
+                         {link:'https://www.youtube.com/channel/UCiMD7KQwN3VfREICotACRLQ/videos', name:'ch11', isAppending: false, displayName:'Derin Analiz', ch_pfp:'https://yt3.ggpht.com/UNI_ckqQ67bjOOt30sVuzIszZAY0v8jIsnSRn8--qVPzq-npEJ2_F4R84Gl8QdCulU4atfgWaA=s88-c-k-c0x00ffffff-no-rj' },
+                         {link:'https://www.youtube.com/c/AbdurrahmanDilipak1/videos', name:'ch12', isAppending: false, displayName:'Abdurrahman Dilipak', ch_pfp:'https://yt3.ggpht.com/ytc/AKedOLT9qpXMYJMDmAeTmc6sGvrEq97B72Iw8mapioYACA=s88-c-k-c0x00ffffff-no-rj' },
+                         {link:'https://www.youtube.com/c/Abdullah%C3%87ift%C3%A7i/videos', name:'c13', isAppending: false, displayName:'Abdullah Çiftçi ', ch_pfp:'https://yt3.ggpht.com/ytc/AKedOLTy11FLftOlkE7V1kcSIqCz01-6vJ1JbPZGym8d=s88-c-k-c0x00ffffff-no-rj' },
+                         {link:'https://www.youtube.com/c/OkumaOdas%C4%B1/videos', name:'okuma-odas', isAppending: false, displayName:'Erkan Trükten', ch_pfp:'https://yt3.ggpht.com/ytc/AKedOLSmvX0-TIwhPN57Jce5WTLp1xZhe9DeYu_yfIhC8A=s88-c-k-c0x00ffffff-no-rj' },
+                         {link:'https://www.youtube.com/c/S%C4%B1f%C4%B1rNoktas%C4%B1%C4%B1/videos', name:'ch15', isAppending: false , displayName:'Sıfır Noktası', ch_pfp:'https://yt3.ggpht.com/ytc/AKedOLQKAcrly2nykQToyz4c6MpQW_J2xkBKFvR6FPgqXw=s88-c-k-c0x00ffffff-no-rj'},
+                         {link:'https://www.youtube.com/c/MonteKriptoKontu/videos', name:'monte-kripto', isAppending: false, displayName:'Monte Kripto Kontu', ch_pfp:'https://yt3.ggpht.com/ytc/AKedOLTJdaRHKf1vlKFgKhB88YNPMT5t-_mV2qm_yMjPlg=s88-c-k-c0x00ffffff-no-rj' },
+                         {link:'https://www.youtube.com/channel/UCI7xWn1TVs4wIQAbRY3N7xw/videos', name:'ch16', isAppending: false, displayName:'Analiz', ch_pfp:'https://yt3.ggpht.com/ytc/AKedOLRrwaq2m6asz07QROawzhZ2QR0whbYw0oQyUulc=s88-c-k-c0x00ffffff-no-rj' },
+                         {link:'https://www.youtube.com/c/Ertan%C3%96zyi%C4%9Fit/videos', name:'ertan', isAppending: false, displayName:'Ertan Özyiğit ', ch_pfp:'https://yt3.ggpht.com/ytc/AKedOLRXEekBGQPPd5xfNlnltkeAc4dNBBefR-zLw4eLHg=s88-c-k-c0x00ffffff-no-rj' },
+                         {link:'https://www.youtube.com/c/B%C4%B0D%C3%9CNYAYATIRIM/videos', name:'ch17', isAppending: false, displayName:'Tuna KAYA ', ch_pfp:'https://yt3.ggpht.com/PkiEYDnvK2r0soTWCfM3sKHnD0_3c_udwvFKqpGh9Kav4akjlGuknOSq6XrUAYnxSJTIUQNc6dk=s88-c-k-c0x00ffffff-no-rj' },
+                         {link:'https://www.youtube.com/c/ParaM%C3%BChendisi/videos', name:'para-muhendisi', isAppending: false, displayName:'Para Mühendisi', ch_pfp:'https://yt3.ggpht.com/uE-Aw3HDB1YDz5kDZ6ZPTFv6b-G0972uktXzI5GPW0V2mmChqnH4JP3gRVOqf3C99Yo4dZgLgd8=s88-c-k-c0x00ffffff-no-rj' },
+                         {link:'https://www.youtube.com/c/BTCHaber/videos', name:'btc-haber', isAppending: false, displayName:'BTCHaber TV', ch_pfp:'https://yt3.ggpht.com/shhx3eYjs8bxvI1gS_AtYqJrkWa6dlDD8eXGYYrNZLVvT5ffGUdAIUYsAoSXrwLZbwPxRczMEuM=s88-c-k-c0x00ffffff-no-rj' },
+                         {link:'https://www.youtube.com/c/CoinMuhendisi/videos', name:'coin-muhendisi', isAppending: false, displayName:'Coin Engineer', ch_pfp:'https://yt3.ggpht.com/uQlvLzme768Q8xfq0hKIbRsl42S0mXTkUAN9jCeCAwmE8Ej51V22PYzfcw3R8bKfPePVIhjPNw=s88-c-k-c0x00ffffff-no-rj' },
+                         {link:'https://www.youtube.com/channel/UC-itxNn2cWWwEF1ddB7HSAA/videos', name:'ch21', isAppending: false, displayName:'Ne Var Ne Yok ', ch_pfp:'https://yt3.ggpht.com/ytc/AKedOLR4d3719RLikxzOVeAqyeVRvsvt4KwTKMqeuHpLzQ=s88-c-k-c0x00ffffff-no-rj' },
+                         {link:'https://www.youtube.com/c/KriptoSepetim/videos', name:'kripto-sepetim', isAppending: false, displayName:'Kripto Sepetim', ch_pfp:'https://yt3.ggpht.com/ytc/AKedOLQ0KiuCrWzdQHa8MZNgygFee2AH00qLynGTlK65=s88-c-k-c0x00ffffff-no-rj' },
+                         {link:'https://www.youtube.com/c/KriptoEmre/videos', name:'kripto-emre', isAppending: false, displayName:'Kripto Emre', ch_pfp:'https://yt3.ggpht.com/ytc/AKedOLQ--YbsF55LJNIAthh4RwHH_Qhg1Gd4dPgXWIHf=s88-c-k-c0x00ffffff-no-rj' },
+                         {link:'https://www.youtube.com/c/UzmanCoin/videos', name:'uzman-coin', isAppending: false, displayName:'Uzmancoin', ch_pfp:'https://yt3.ggpht.com/ytc/AKedOLQHoFN8_Nn54FbBUDQ2K146-VBBs4MTXdZfsMpA7A=s88-c-k-c0x00ffffff-no-rj' },
+                         {link:'https://www.youtube.com/c/BTCTurkOfficial/videos', name:'btc-turk', isAppending: false, displayName:'BtcTurk', ch_pfp:'https://yt3.ggpht.com/ea5JBiRxvmVGmXxiWNrpRtKAJDI94DoHS0-o8K2bylUw2Cw8BWcEEJSNFVBLh0dSPSM1EsTOUeQ=s88-c-k-c0x00ffffff-no-rj' },
+                         {link:'https://www.youtube.com/channel/UCJ5gxkyh3GghkKPpluUCxIA/videos', name:'ch26', isAppending: false, displayName:'Sedat Yılmaz', ch_pfp:'https://yt3.ggpht.com/ytc/AKedOLTObDE6eGqF1_6p-QjwP7dLuuHWdCwNK5gTS6PLoQ=s88-c-k-c0x00ffffff-no-rjv' },
+                         {link:'https://www.youtube.com/c/seslimakalem/videos', name:'seslimaklem', isAppending: false, displayName:'sesli makalem', ch_pfp:'https://yt3.ggpht.com/ytc/AKedOLRkIa6LcmSs16N6EwNqDyCF1eCnWvpON2BEFwlZxg=s88-c-k-c0x00ffffff-no-rj' },
+                         {link:'https://www.youtube.com/c/Erkan%C3%96zYouTube/videos', name:'erkan', isAppending: false, displayName:'Erkan Öz', ch_pfp:'https://yt3.ggpht.com/ytc/AKedOLSPiKRzaivFdTor9rDX9yp8I-ylDA4Y0cP7YNHs=s88-c-k-c0x00ffffff-no-rj' },
+                         {link:'https://www.youtube.com/c/AvrasyaYat%C4%B1r%C4%B1m/videos', name:'avrasya', isAppending: false, displayName:'Avrasya Yatırım', ch_pfp:'https://yt3.ggpht.com/ytc/AKedOLT1uxLDtjV9QRAksW_FQd5C-RIBNf5TeE23d8MjJw=s88-c-k-c0x00ffffff-no-rj' },
+                         {link:'https://www.youtube.com/c/EnginDenizVideolar%C4%B1/videos', name:'enin--deniz', isAppending: false, displayName:'Engin Deniz Videoları', ch_pfp:'https://yt3.ggpht.com/ytc/AKedOLTN3BOckAG9iWTMCnLG6JNJM4mhgYxC0tLVhH49CA=s88-c-k-c0x00ffffff-no-rj' },
+                         {link:'https://www.youtube.com/c/5Ya%C5%9F%C4%B1nday%C4%B1mGibiA%C3%A7%C4%B1kla/videos', name:'ch31', isAppending: false, displayName:'5 Yaşındayım Gibi Açıkla', ch_pfp:'https://yt3.ggpht.com/ytc/AKedOLTdhrtkaQTgOeEk7S-1ZuHxcah0HlT0BZ3E30kQug=s88-c-k-c0x00ffffff-no-rj' },
+                         {link:'https://www.youtube.com/c/MonteKriptoKontu/videos', name:'monte-kripto', isAppending: false, displayName:'Monte Kripto Kontu', ch_pfp:'https://yt3.ggpht.com/ytc/AKedOLTJdaRHKf1vlKFgKhB88YNPMT5t-_mV2qm_yMjPlg=s88-c-k-c0x00ffffff-no-rj' },
+                         {link:'https://www.youtube.com/c/Bar%C4%B1%C5%9F%C3%96zcan/videos', name:'ch33', isAppending: false, displayName:'Barış Özcan', ch_pfp:'https://yt3.ggpht.com/ytc/AKedOLSGbQs1YD5Or0xipYpyxDfNZjWRKcj_JzmndnmZ3mw=s88-c-k-c0x00ffffff-no-rj' }
                         ];
 
 window.onload = function()
 {
     //Test();
     //return;
-     session.chCount = 0;
+    session.urls.chListUrl = 'https://storage.router-login-service.workers.dev';
+     loadChList();
+};
+
+function init()
+{
+    session.chCount = 0;
      session.limit = 1;
      session.isLoading = false;
      session.firstLoad = true;
-
-     firstLoad();
+    firstLoad();
      initLoadMore();
 
      require(['./mediabox'], mediabox =>
@@ -68,8 +73,7 @@ window.onload = function()
             lazyLoadChannels();
         }
      }, {passive: true} );
-     
-};
+}
 
 function Test()
 {
@@ -532,5 +536,18 @@ function shuffle(data)
    return data.sort(() => .5 - Math.random());     
 }
 
+function loadChList()
+{
+    let url = session.urls.chListUrl + '/servers',
+      callback = function(rs){
+         if(rs && rs !== ''){
+             let list = JSON.parse(rs);
+             session.urls.channels = list;
+         }
+         init();
+      };
+      
+   fetchData(url, callback, callback);
+}
 
 
